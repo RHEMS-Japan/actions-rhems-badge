933f6b7ddbeaa69b787c4ce000fd49900a092da8
RayMeibergen <rayman@rhems-japan.co.jp>
2021-11-24 14:46:38 +0900

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,18 @@
 #!/bin/sh -l
 #### made by rayman@rhems-japan.co.jp
 
+_BRANCH=${GITHUB_REF_NAME}
+[ -n ${INPUT_BRANCH} ] && _BRANCH=${INPUT_BRANCH}
 
 echo "==== debug ===="
 cat << EOS
+txt : ${_BRANCH} / ${INPUT_BRANCH}
 {
   "api_token": "${INPUT_API_TOKEN}",
   "organization": "${GITHUB_REPOSITORY_OWNER}",
   "repo": "$(echo $GITHUB_REPOSITORY | rev | cut -d '/' -f 1 | rev)",
   "app": "${INPUT_APP}",
-  "branch": "${GITHUB_REF_NAME}",
+  "branch": "${_BRANCH}",
   "status": "${INPUT_STATUS}",
 `[ -n "${INPUT_TXT}" ] && \
 cat << EOF
@@ -19,9 +22,6 @@ EOF`
 }
 EOS
 echo "==== debug ===="
-
-_BRANCH=${GITHUB_REF_NAME}
-[ -n ${INPUT_BRANCH} ] && _BRANCH=${INPUT_BRANCH}
 
 curl -X POST -H "Content-Type: application/json" \
              https://badges.rhems-japan.com/api-update-badge \
