83491121a1ae28d7ca0141ad2249f68a4a48610c
RayMeibergen <rayman@rhems-japan.co.jp>
2021-11-24 14:36:23 +0900

    rayman did

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,24 @@
 #!/bin/sh -l
 #### made by rayman@rhems-japan.co.jp
-# echo "===="
-# cat << EOS
-# {
-#   "api_token": "${INPUT_API_TOKEN}",
-#   "organization": "${GITHUB_REPOSITORY_OWNER}",
-#   "repo": "$(echo $GITHUB_REPOSITORY | rev | cut -d '/' -f 1 | rev)",
-#   "app": "${INPUT_APP}",
-#   "branch": "${GITHUB_REF_NAME}",
-#   "status": "${INPUT_STATUS}",
-# `[ -n "${INPUT_TXT}" ] && \
-# cat << EOF
-#   "txt": "${INPUT_TXT}",
-# EOF`
-#   "update": "$(date "+%Y-%m-%d-%H:%M:%S")"
-# }
-# EOS
-# env
-# echo "===="
+
+
+echo "==== debug ===="
+cat << EOS
+{
+  "api_token": "${INPUT_API_TOKEN}",
+  "organization": "${GITHUB_REPOSITORY_OWNER}",
+  "repo": "$(echo $GITHUB_REPOSITORY | rev | cut -d '/' -f 1 | rev)",
+  "app": "${INPUT_APP}",
+  "branch": "${GITHUB_REF_NAME}",
+  "status": "${INPUT_STATUS}",
+`[ -n "${INPUT_TXT}" ] && \
+cat << EOF
+  "txt": "${INPUT_TXT}",
+EOF`
+  "update": "$(date "+%Y-%m-%d-%H:%M:%S")"
+}
+EOS
+echo "==== debug ===="
 
 _BRANCH=${GITHUB_REF_NAME}
 [ -n ${INPUT_BRANCH} ] && _BRANCH=${INPUT_BRANCH}
